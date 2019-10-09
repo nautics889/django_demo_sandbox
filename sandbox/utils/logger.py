c0c979da11efff05e4d133a27cc26c231a8f5cd5
nautics889 <cyberukr@gmail.com>
2019-10-09 23:02:44 +0300

    add (dockerfile, docker-compose): initialized; add (django project): initialized project, created basic structure; add (logger): created general logger; add (dependencies): requirements.txt

---
+++ sandbox/utils/logger.py
@@ -0,0 +1,8 @@
+import logging
+
+
+logging.basicConfig(format='%(asctime)s  %(levelname)-8s '
+                           '[%(filename)s:%(lineno)d] %(message)s',
+                    datefmt='%m/%d/%Y %I:%M:%S')
+LOGGER = logging.getLogger()
+LOGGER.setLevel(logging.DEBUG)
