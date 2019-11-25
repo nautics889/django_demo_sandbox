281bb51519508c57f59b0ce3c54a0cfb91285ed2
nautics889 <cyberukr@gmail.com>
2019-11-25 18:45:31 +0200

    updated (user_app): tests divided into different files; fix (user_app): fixed wrong assertion in ``SandyCurrentUserDetailsTest``;

--- sandbox/user_app/tests/test_current_user_details.py
+++ sandbox/user_app/tests/test_current_user_details.py
@@ -2,22 +2,7 @@ from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase, APIClient
 
-from user_app.models import SandyUser
 from user_app.tests import factories
-
-
-class CreateSandyUserTest(APITestCase):
-    def setUp(self):
-        self.url = reverse('list-create-users')
-        user_instance = factories.SandyUserFactory.build()
-        self.data = dict(username=user_instance.username,
-                         email=user_instance.email,
-                         password=user_instance.password)
-
-    def test_valid_data(self):
-        response = self.client.post(self.url, self.data, format='json')
-        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-        self.assertTrue(SandyUser.objects.get(email=self.data['email']))
 
 
 class SandyCurrentUserDetailsTest(APITestCase):
@@ -43,4 +28,4 @@ class SandyCurrentUserDetailsTest(APITestCase):
         response = self.client.get(self.url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data['username'], self.user.username)
-        self.assertEqual(response.data['email'], self.user.username)
+        self.assertEqual(response.data['email'], self.user.email)
