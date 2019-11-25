b18c62ecb012d4d8d51c2cf1bede1a2a2751bc91
nautics889 <cyberukr@gmail.com>
2019-11-25 18:30:33 +0200

    fix (user_app): solved issues with token-patching for tests which require authentication; add (user_app): add checking of entity appearing in ``CreateSandyUserTest``;

--- sandbox/user_app/tests/tests.py
+++ sandbox/user_app/tests/tests.py
@@ -2,8 +2,8 @@ from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase, APIClient
 
-from user_app.tests import factories
 from user_app.models import SandyUser
+from user_app.tests import factories
 
 
 class CreateSandyUserTest(APITestCase):
@@ -17,32 +17,30 @@ class CreateSandyUserTest(APITestCase):
     def test_valid_data(self):
         response = self.client.post(self.url, self.data, format='json')
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertTrue(SandyUser.objects.get(email=self.data['email']))
 
 
 class SandyCurrentUserDetailsTest(APITestCase):
     client = APIClient()
-    token = 'test123455'
+    token = 'test12345'
 
     @classmethod
     def setUpTestData(cls):
         cls.user = factories.SandyUserFactory.create()
         cls.user.set_password(cls.user.password)
         cls.user.save()
-        cls.user.oauth2_provider_accesstoken.create(
+        token = cls.user.oauth2_provider_accesstoken.create(
             expires='2050-12-31 23:59:59', token=cls.token
         )
-        import pdb
-        pdb.set_trace()
+        token.scope = 'read write'
+        token.save()
 
     def setUp(self):
         self.url = reverse('retrive-details-current-user')
-        # TODO: make more correct user creation (via factory's method)
         self.client.credentials(Authorization='Bearer {}'.format(self.token))
 
-    def test_ensure_current_user(self):
-        import pdb
-        pdb.set_trace()
-        pass
-        # response = self.client.get(self.url)
-        # self.assertEqual(response.status_code, status.HTTP_200_OK)
-        # self.assertEqual(response.body)
+    def test_current_user_info(self):
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['username'], self.user.username)
+        self.assertEqual(response.data['email'], self.user.username)
