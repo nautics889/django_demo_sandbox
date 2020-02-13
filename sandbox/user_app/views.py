570aa8bb04ad05a51d1c661fcc06973231faea7a
nautics889 <cyberukr@gmail.com>
2020-02-13 22:31:16 +0200

    updated (user_app): removed several line in views.py;

--- sandbox/user_app/views.py
+++ sandbox/user_app/views.py
@@ -3,13 +3,12 @@ from rest_framework import generics, permissions
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from django.utils.encoding import force_text
-from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.utils.http import urlsafe_base64_decode
 from user_app.token import account_activation_token
 
 from user_app.models import SandyUser
 from user_app.serializers import SandyUserSerializer
-
-# TODO: update view for signup, create token confirm view
+from utils import LOGGER
 
 
 class SandyUserList(generics.ListCreateAPIView):
@@ -34,6 +33,7 @@ def activate_account(request):
         uid = force_text(urlsafe_base64_decode(encoded_uid))
         user = SandyUser.objects.get(pk=uid)
     except (TypeError, ValueError, OverflowError, SandyUser.DoesNotExist) as e:
+        LOGGER.debug(e, exc_info=True)
         user = None
     if user is not None and account_activation_token.check_token(user, token):
         user.is_active = True
